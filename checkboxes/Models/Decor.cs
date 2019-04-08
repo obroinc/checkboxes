fae3e686a99347019975f4160a64aed3e19ce235
HPLaptop <HPLaptop@CLEMENTLAPTOP>
2019-04-08 18:36:51 +0100

    Add project files.

---
+++ checkboxes/Models/Decor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace checkboxes.Models
+{
+    public class Decor
+    {
+
+        public int ID { get; set; }
+
+        public bool IsRedChecked { get; set; }
+
+        public bool IsBlueChecked { get; set; }
+
+        public bool IsGreenChecked { get; set; }
+
+        public int GetNumberOfColours ()
+        {
+
+            int count = 0;
+
+            if (IsRedChecked)
+            {
+                count++;
+            }
+            if (IsBlueChecked)
+            {
+                count++;
+            }
+            if (IsGreenChecked)
+            {
+                count++;
+            }
+
+            return count;
+
+        }
+    }
+}
